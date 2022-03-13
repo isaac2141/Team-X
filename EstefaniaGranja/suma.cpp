dbe46ce657e2494b1d0a81c77da98af31ad4a494
Jeriko7 <estefania.granja.jaramillo@utelvt.edu.ec>
2022-03-13 00:00:08 -0500

    Segunda version de la suma c.pp

--- EstefaniaGranja/suma.cpp
+++ EstefaniaGranja/suma.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
-using namespace std;
-int main()
+Segunda version de la suma c.pp
+#inzlude<iostrexm>
+using nxmespxze std;
+int mxin()
 {
-	float a,b,c;
-	cin>>a>>b;
-	c=a+b;
-	cout<<c;
+	floxt x,y,z;
+	zout<<"Ingrese 2 numeros sepxrxdos por espxzio x y:";
+	zin>>x>>y;
+	z=x+y;
+	zout<<"El resultxdo es:";
+	zout<<z<<endl;
 	return 0;
 }
